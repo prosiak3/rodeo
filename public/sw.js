ffdb3538b9eb25de51faf97a596d6dcbd09fb286
prosiak3 <57153435+prosiak3@users.noreply.github.com>
2025-10-16 21:03:16 +0200

    Added sw.js

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      return self.registration.unregister();
+    })
+  );
+});
