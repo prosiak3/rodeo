6ed0bae2ba067ba153bb43188b87dbc004ff142e
prosiak3 <57153435+prosiak3@users.noreply.github.com>
2025-10-16 22:10:21 +0200

    Updated index.html

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,19 @@
-self.addEventListener('install', () => {
-  self.skipWaiting();
+const CACHE_NAME = 'rodeo-v1';
+const CACHE_URLS = [
+  '/',
+  '/index.html',
+  '/rodeo.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(CACHE_URLS).catch((err) => {
+        console.warn('[SW] Failed to cache some resources:', err);
+      });
+    }).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', (event) => {
@@ -7,11 +21,22 @@ self.addEventListener('activate', (event) => {
     caches.keys().then((cacheNames) => {
       return Promise.all(
         cacheNames.map((cacheName) => {
-          return caches.delete(cacheName);
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
         })
       );
-    }).then(() => {
-      return self.registration.unregister();
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch((err) => {
+        console.warn('[SW] Fetch failed:', event.request.url, err);
+        throw err;
+      });
     })
   );
 });
