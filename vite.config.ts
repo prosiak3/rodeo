2fc5ed99f0fa8a7854d29d79547a34c33be715b3
prosiak3 <57153435+prosiak3@users.noreply.github.com>
2025-10-16 15:57:16 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL || 'https://zpbhwjnuqiomuufscvho.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwYmh3am51cWlvbXV1ZnNjdmhvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwNjkxNDUsImV4cCI6MjA3NTY0NTE0NX0.S1zwrdxL6L9gzGIORwMk00CVNM8_tg5EnLMGwqsWmks'),
+  },
   optimizeDeps: {
     exclude: ['@xenova/transformers'],
     include: ['onnxruntime-web'],
